2e91a183466da28fb319a85e19c9669b405729d7
Ashwin <ashwinnagelwar@gmail.com>
2021-10-09 13:12:31 +0530

    IES-1:String s1 created

--- src/main/java/com/BikkadIT/GitBranchApplication.java
+++ src/main/java/com/BikkadIT/GitBranchApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitBranchApplication {
 
+	String s1="Ashwin";
 	public static void main(String[] args) {
 		SpringApplication.run(GitBranchApplication.class, args);
 	}
